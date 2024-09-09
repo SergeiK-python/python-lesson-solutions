30d172e29a3e5e39d5bc779bdb4c8955cc539277
SergeiK-python <srgkuptsov@yandex.ru>
2024-09-09 17:22:03 +0300

    Delete modules/modules directory

--- modules/modules/module_1_5.py
+++
@@ -1,8 +0,0 @@
-#Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"
-
-immutable_var = ([1,2,3], True, "Cat")
-print(immutable_var)
-#immutable_var[1] = False
-mutable_var = [[1,2,3], True, "Cat"]
-mutable_var[2] = "Dog"
-print(mutable_var)
