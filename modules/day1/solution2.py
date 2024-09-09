df6582698cdc2806bffc2d23c6f41459ea66b36d
SergeiK-python <srgkuptsov@yandex.ru>
2024-09-09 17:21:35 +0300

    Delete modules/day1 directory

--- modules/day1/solution2.py
+++
@@ -1,8 +0,0 @@
-#Практическое задание по уроку "Строки и индексация строк"
-
-example : str = "Топинамбур" #"Urban" "hello my little big adventure"
-print(example[0])
-print(example[-1])
-print(example[len(example) // 2:])
-print(example[::-1])
-print(example[1::2])
