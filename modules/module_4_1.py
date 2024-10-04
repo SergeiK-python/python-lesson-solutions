0343fa833f4c53562091c2878ece493b6c926125
SergeiK-python <srgkuptsov@yandex.ru>
2024-10-04 10:21:20 +0300

    module_4_1.py and its files in ./math

---
+++ modules/module_4_1.py
@@ -0,0 +1,14 @@
+# Домашняя работа по уроку "Модули и пакеты"
+
+#  by absolute paths
+from modules.math.fake_math import divide as fake_divide
+from modules.math.true_math import divide as true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
