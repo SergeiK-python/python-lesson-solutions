a88f429e286c1c1f152f238d99cbf240a96812d9
SergeiK-python <srgkuptsov@yandex.ru>
2024-10-12 21:30:27 +0300

    5_1, 5_2

---
+++ modules/module_5_1.py
@@ -0,0 +1,19 @@
+#  Домашняя работа по уроку "Атрибуты и методы объекта."
+
+class House:
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        if 1 <= new_floor <= self.number_of_floors:
+            for ii in range(1, new_floor + 1):
+                print(ii)
+        else:
+            print("Такого этажа не существует")
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
