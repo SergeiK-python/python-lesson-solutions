5df7cd58767a5e73c2e1aa2d3c28b25ae540ea9a
SergeiK-python <srgkuptsov@yandex.ru>
2025-01-20 03:13:04 +0300

    15_1 15_2 (links)

---
+++ modules/module_15_1.py
@@ -0,0 +1,3 @@
+# Домашнее задание по теме "История и развитие ИИ"
+
+# https://colab.research.google.com/drive/18wmhUaLUP_QJwGm87pVbAJe8B3bgg9VM?usp=sharing
---
+++ modules/module_15_2.py
@@ -0,0 +1,3 @@
+# Домашнее задание по теме "Знакомство и работа с GoogleColab"
+
+# https://colab.research.google.com/drive/1DeVENyNjNO_BnjJADY8PM2-d_0rRQaJR?usp=sharing
